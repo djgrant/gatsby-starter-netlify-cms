227eff30d611a9440ea8be696563f7303fb9de91
Daniel Grant <daniel.grant@skyscanner.net>
2019-08-11 21:18:00 +0100

    Remove landing page CMS template

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,4 @@
 import CMS from 'netlify-cms-app';
-
 import AboutPagePreview from './preview-templates/about-page-preview';
-import IndexPagePreview from './preview-templates/index-page-preview';
 
-CMS.registerPreviewTemplate('index', IndexPagePreview);
 CMS.registerPreviewTemplate('about', AboutPagePreview);
