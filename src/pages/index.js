1efdacbf2b866280676f0d22ef45c9a8df6ba875
Addis Tour Guide <110671638+addistourguide@users.noreply.github.com>
2022-09-10 17:45:40 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,8 +7,7 @@ import Card from '../components/atoms/card';
 
 export default () => (
   <Layout>
-           <img src="/img/WhatsApp Image 2022-09-07 at 3.47.59 PM.jpeg "
-                     ="/img/WhatsApp Image 2022-09-07 at 3.48.09 PM.jpeg "  />
+          <jumbotron <img src="/img/WhatsApp Image 2022-09-07 at 3.47.59 PM.jpeg < /img/WhatsApp Image 2022-09-07 at 3.48.09 PM.jpeg "  />
     <Box container gutter>
       <Article pt={[4, 4]} pb={[4, 5]}>
         <h2>Personal tour operator with a trusted independent guide</h2>
